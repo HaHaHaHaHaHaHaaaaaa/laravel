c8131ddcf376925421fe7f74902f6ffab8b8553d
wbl <651753175@qq.com>
2017-11-11 16:53:47 +0800

    model test

---
+++ app/User_php.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User_php extends Model
+{
+    //
+    protected $table="user";
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 use Illuminate\Support\Facades\DB;
+use App\User_php;
 
 Route::get('/', function () {
     
@@ -20,18 +21,30 @@ Route::get('/', function () {
 Route::get('/user', function () {
      $users=DB::table('user')->get();
      $arr=[];
-     foreach ($users as $key => $value) {
-         array_push($arr,$value);
-     }
-     $target=DB::table('user')->where('username','wbl')->get();
+    foreach ($users as $key => $value) {
+        array_push($arr, $value);
+    }
+     $target=DB::table('user')->where('username', 'wbl')->get();
     return json_encode($arr);
 });
 
-Route::get('/test',function(){
+Route::get('/test', function () {
     $users=DB::table('user')->get();
     $arr=[];
     foreach ($users as $key => $value) {
-        array_push($arr,$value);
+        array_push($arr, $value);
     }
-    return view('test')->with('ct',$arr);
+    return view('test')->with('ct', $arr);
+});
+
+
+
+
+
+Route::get('/model', function () {
+    $user= App\User_php::all();
+    foreach ($user as $obj) {
+        echo $obj->username;
+        echo $obj->password;
+    }
 });
